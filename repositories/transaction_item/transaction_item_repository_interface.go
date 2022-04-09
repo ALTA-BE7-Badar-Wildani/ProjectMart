70d5e42ea37bd51a9850a90cf998e4d93970ca5d
Badar Wildani <wildanie12@gmail.com>
2022-04-09 22:41:33 +0700

    feat: merge cart item if cart product already exists

--- repositories/transaction_item/transaction_item_repository_interface.go
+++ repositories/transaction_item/transaction_item_repository_interface.go
@@ -6,6 +6,7 @@ type TransactionItemRepositoryInterface interface {
 	FindAll(limit int, offset int, filters []map[string]string, sorts []map[string]interface{}) ([]entityDomain.TransactionItem, error)
 	Find(id int) (entityDomain.TransactionItem, error)
 	FindBy(field string, value string) (entityDomain.TransactionItem, error)
+	FindFirst(filters []map[string]string) (entityDomain.TransactionItem, error)
 	CountAll() (int64, error)
 	Store(transactionItem entityDomain.TransactionItem) (entityDomain.TransactionItem, error)
 	Update(transactionItem entityDomain.TransactionItem, id int) (entityDomain.TransactionItem, error)
